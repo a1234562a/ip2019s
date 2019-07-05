dd77f63a7d0d5296824c82c79e9be44aff9955b9
a1234562a <52517958+a1234562a@users.noreply.github.com>
2019-07-05 18:02:16 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,39 @@
 
 
 function start(e) {
+    var name=[
+    {
+        char:'饒',
+        big5:'C4C7',
+        cns:'1-7A52',
+        unicode:'9952'
+    },
+    {
+        char:'奇',
+        big5:'A95F',
+        cns:'1-4C61',
+        unicode:'5947'
+    },
+    {
+        char:'軒',
+        big5:'B061',
+        cns:'1-5846',
+        unicode:'8ED2'
+    }
+     ];
 
+     var ele = document.getElementById("div1");
+
+     for (var i=0;i < 3; i++) {
+          ele.innerHTML += name[i].char;
+          ele.innerHTML += ' big5 =';
+          ele.innerHTML += name[i].big5;
+          ele.innerHTML += ' cns =';
+          ele.innerHTML += name[i].cns;
+          ele.innerHTML += ' unicode =';
+          ele.innerHTML += name[i].unicode;
+          ele.innerHTML += '<br>';
+    }
 }
 
 /*
